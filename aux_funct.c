ac25fadb352ddf014103c45b2717ed8d32c3185e
Dannyelgeek <daot1993@gmail.com>
2023-01-06 16:57:23 -0500

    sorting algorithms task 3

--- aux_funct.c
+++ aux_funct.c
@@ -41,37 +41,3 @@ void print_list(const listint_t *list)
 	}
 	printf("\n");
 }
-
-/**
- * 
-*/
-
-int division(int *array, int left, int right, size_t size)
-{
-	int piv = array[right], x = left - 1, y, temp = 0;
-
-	for (y = left; y <= right - 1; y++)
-	{
-		if (array[y] <= piv)
-		{
-			x++;
-			if (x != y)
-			{
-				temp = array[x];
-				array[x] = array[y];
-				array[y] = temp;
-				print_array(array, size);
-			}
-		}
-	}
-
-	temp = 0;
-	if (array[right] < array[x + 1])
-	{
-		temp = array[x + 1];
-		array[x +1] = array[right];
-		array[right] = temp;
-		print_array(array, size);
-	}
-	return (x + 1);
-}
