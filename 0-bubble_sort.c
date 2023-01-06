dd86b9d4ff5cd3ca2e536ce5ee1c4918ac41944f
Dannyelgeek <daot1993@gmail.com>
2023-01-06 14:50:34 -0500

    sorting algorithms task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0, j = 0, temp;
 
-	if (size < 2 || !array)
+	if (size < 2)
 		return;
 
 	while (i < size - 1)
@@ -20,8 +20,8 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = &array[j];
-				array[j] = &array[j + 1];
+				temp = array[j];
+				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
 			}
